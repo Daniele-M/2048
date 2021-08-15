26890bef7e18609663491fc24a0278a696567894
Daniele-M <79785038+Daniele-M@users.noreply.github.com>
2021-08-15 16:54:34 +0200

    Add files via upload

--- 2048/logic.py
+++ 2048/logic.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import numpy as np
 import random
+from os import path
 
 from settings import *
 
@@ -8,13 +9,27 @@ from settings import *
 class Board:
     def __init__(self, game):
         self.game = game
+        self.game_square = 4
+        self.cellsize = int(WIDTH / self.game_square)
         self.game_over = False
-        self.b = np.zeros((GAME_SQUARE, GAME_SQUARE))
-        self.font = pg.font.SysFont('arial', CELL_SIZE//5)
-        self.previus_move = np.zeros((GAME_SQUARE, GAME_SQUARE))
+        self.b = np.zeros((self.game_square, self.game_square))
+        self.previus_move = np.zeros((self.game_square, self.game_square))
+        self.score = 0
+        self.rect_width = WIDTH/2 - 70
+        self.score_rect = pg.Rect(20, 100, self.rect_width, 50)
+        self.record_rect = pg.Rect(WIDTH/2 + 70 - 20, 100, self.rect_width, 50)
+
+    def draw_UI(self):
+        self.display("Score", 35, 20 + self.rect_width/2, 60)
+        self.display("Record", 35, WIDTH/2 + 70 - 20 + self.rect_width/2, 60)
+        pg.draw.rect(self.game.screen, UI_COLOR, self.score_rect, border_radius=15)
+        pg.draw.rect(self.game.screen, UI_COLOR, self.record_rect, border_radius=15)
+        self.display(str(self.score), 25,  self.score_rect.centerx, self.score_rect.centery)
+        self.display(str(self.game.record), 25, self.record_rect.centerx, self.record_rect.centery)
+
 
     def draw_lines(self):
-        n = GAME_SQUARE
+        n = self.game_square
         pg.draw.line(self.game.screen, LINE_COLOR, (0, MARGIN), (WIDTH, MARGIN), 5)
         #Orizzontal lines
         for i in range(1, n):
@@ -23,38 +38,39 @@ class Board:
         for i in range(1, n):
             pg.draw.line(self.game.screen, LINE_COLOR, (i * WIDTH/n, MARGIN), (i * WIDTH/n, MARGIN + HEIGHT), 5)
 
-    def display(self, text, rect):
-
-        txt = self.font.render(text, True, BLACK)
-        text_rect = txt.get_rect(center=(rect.centerx,rect.centery))
-        self.game.screen.blit(txt, text_rect)
+    def display(self, text, font_size, x, y):
+        # x,y of the center
+        font = pg.font.SysFont('arial', max(15, font_size))
+        text_surface = font.render(text, True, BLACK)
+        text_rect = text_surface.get_rect(center=(x, y))
+        self.game.screen.blit(text_surface, text_rect)
 
     def draw_nums(self):
-        for row in range(GAME_SQUARE):
-            for col in range(GAME_SQUARE):
+        for row in range(self.game_square):
+            for col in range(self.game_square):
                 if self.b[row][col] != 0:
                     value = int(self.b[row][col])
                     text = str(value)
-                    position = (col * CELL_SIZE + 3, MARGIN + row * CELL_SIZE + 3)
-                    rect = pg.Rect(position, (CELL_SIZE - 5, CELL_SIZE - 5))
+                    position = (col * self.cellsize + 3, MARGIN + row * self.cellsize + 3)
+                    rect = pg.Rect(position, (self.cellsize - 5, self.cellsize - 5))
                     pg.draw.rect(self.game.screen, COLORS[value], rect)
-                    self.display(text, rect)
+                    self.display(text, self.cellsize//5, rect.centerx, rect.centery)
 
-    def move_cells(self, direction):
-        temp = np.copy(self.b)
+    def move_cells(self, direction, board, original_board=True):
+        temp = np.copy(board)
         moved = False
         skip = False
 
-        for i in range(GAME_SQUARE):
+        for i in range(self.game_square):
             if direction in "leftright":
-                vec = self.b[i, :]
+                vec = board[i, :]
                 if direction == "right":
                     vec = vec[::-1]
             else:
-                vec = self.b[:, i]
+                vec = board[:, i]
                 if direction == "down":
                     vec = vec[::-1]
-            for j in range(GAME_SQUARE):
+            for j in range(self.game_square):
                 if vec[j] != 0:
                     temp_j = j
                     while temp_j > 0 and vec[temp_j-1] == 0:
@@ -70,20 +86,21 @@ class Board:
                             vec[temp_j] = 0
                             skip = True
                             moved = True
+                            if original_board:
+                                self.update_score(int(vec[temp_j-1]))
                     else:
                         skip = False
 
-
-
-        if moved:
+        if moved and original_board:
             self.previus_move = temp
             self.spawn_cells()
             moved = False
+            self.check_game_over()
 
     def spawn_cells(self, num=1):
         free_cells = self.get_available_cells()
         if free_cells == []:
-            self.end_game()
+            self.game_over = True
         else:
             spawn_points = random.sample(free_cells, num)
             for cell in spawn_points:
@@ -95,14 +112,25 @@ class Board:
 
     def get_available_cells(self):
         index = []
-        for row in range(GAME_SQUARE):
-            for col in range(GAME_SQUARE):
+        for row in range(self.game_square):
+            for col in range(self.game_square):
                 if self.b[row][col] == 0:
                     index.append((row, col))
         return index
 
-    def end_game(self):
-        self.game_over = True
 
     def move_back(self):
         self.b = self.previus_move
+
+    def update_score(self, score):
+        self.score += score
+
+    def check_game_over(self):
+        directions = ["left", "right", "up", "down"]
+        copy = self.b.copy()
+        for dir in directions:
+            self.move_cells(dir, copy, original_board=False)
+            v = copy != self.b
+            if v.any():
+                return
+        self.game_over = True
--- 2048/main.py
+++ 2048/main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+from os import path
 
 from settings import *
 from logic import *
@@ -8,21 +9,32 @@ from logic import *
 class Game:
     def __init__(self):
         pg.init()
-
         self.screen = pg.display.set_mode((WIDTH,HEIGHT))
         pg.display.set_caption(TITLE)
         self.board = Board(self)
         self.direction = ""
         self.clock = pg.time.Clock()
         self.running = True
+        self.load_HS()
+
+    def load_HS(self):
+        file = path.join(path.dirname(__file__), 'highscore.txt')
+        if not path.exists(file):
+            with open(file, 'w') as f:
+                f.writelines(["0\n" for _ in range(15)])
+        with open(file, "r+") as f:
+            self.highscores = f.readlines()
 
 
     def new(self):
-        self.score = 0
+        if self.running:
+            self.board.score = 0
+            self.board.game_over = False
+            self.board.cellsize = int(WIDTH / self.board.game_square)
+            self.board.b = np.zeros((self.board.game_square, self.board.game_square))
+            self.board.spawn_cells(2)
 
-        self.board.spawn_cells(2)
-
-        self.run()
+            self.run()
 
     def run(self):
         self.playing = True
@@ -54,22 +66,75 @@ class Game:
     def update(self):
         if self.board.game_over:
             self.playing = False
-            self.running = False
         if self.direction != "":
-            self.board.move_cells(self.direction)
+            self.board.move_cells(self.direction, self.board.b)
         self.direction  = ""
 
     def draw(self):
         self.screen.fill(BG_COLOR)
         self.board.draw_lines()
         self.board.draw_nums()
+        self.board.draw_UI()
 
         pg.display.flip()
 
+    def game_over_screen(self):
+        if not self.running:
+            return
+        self.screen.fill(BG_COLOR)
+        self.board.display("GAME OVER!", 50, WIDTH/2, HEIGHT/4)
+        if self.board.score > self.record:
+            self.record = self.board.score
+            self.highscores[self.board.game_square-2] = f"{self.board.score}\n"
+            self.board.display("NEW RECORD: " + str(self.board.score), 40, WIDTH/2, HEIGHT*3/4)
+            with open(path.join(path.dirname(__file__), "highscore.txt"), 'w') as f:
+                f.writelines(self.highscores)
+        else:
+            self.board.display("SCORE: " + str(self.board.score), 40, WIDTH/2, HEIGHT*3/4)
 
+        self.board.display("HIGHSCORE: " + str(self.record), 40, WIDTH/2, HEIGHT*3/4 + 50)
+        self.board.display("PRESS ENTER TO CONTINUE", 30, WIDTH/2, HEIGHT/2)
+
+        pg.display.flip()
+        self.wait_for_key()
+
+    def wait_for_key(self):
+        waiting = True
+        while waiting:
+            self.clock.tick(FPS)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting = False
+                    self.running = False
+                if event.type == pg.KEYUP:
+                    if event.key == pg.K_RETURN:
+                        waiting = False
+
+    def options_screen(self):
+        selected = False
+        while not selected:
+            self.screen.fill(BG_COLOR)
+            self.board.display("CHOOSE THE BOARD DIMENSIONS", 25, WIDTH/2, HEIGHT/4)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    selected = True
+                    self.running = False
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_LEFT and self.board.game_square > 2:
+                        self.board.game_square -= 1
+                    elif event.key == pg.K_RIGHT and self.board.game_square < 16:
+                        self.board.game_square += 1
+                    elif event.key == pg.K_RETURN:
+                        selected = True
+            self.record = int((self.highscores[self.board.game_square-2]).strip("\n"))
+            self.board.display("RECORD: " + str(self.record), 30, WIDTH/2, HEIGHT/2)
+            self.board.display(f"{str(self.board.game_square)}X{str(self.board.game_square)}", 25, WIDTH/2, HEIGHT*3/4)
+            pg.display.flip()
 
 g = Game()
 while g.running:
+    g.options_screen()
     g.new()
+    g.game_over_screen()
 
 sys.exit()
